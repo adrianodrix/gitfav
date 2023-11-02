aa7d66c6ff866551f5562b3814e2a41471ea76e1
Adriano Santos <adrianodrix@gmail.com>
2023-11-02 19:23:27 -0300

    fix: url github api

--- js/githubuser.js
+++ js/githubuser.js
@@ -1,6 +1,6 @@
 export class GithubUser {
     static async search(username) {
-        const endpoint = `https:/api.github.com/users/${username}`
+        const endpoint = `https://api.github.com/users/${username}`
         try {
             const data = await fetch(endpoint)
             const { login, name, public_repos, followers } = await data.json()
